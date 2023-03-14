e988b93a865a417d9bce721271e21190d4712794
swuY <swu341457@gmail.com>
2023-03-14 21:47:06 +0100

    Site updated: 2023-03-14 21:47:05

--- js/hightPercent.js
+++ js/hightPercent.js
@@ -14,6 +14,20 @@ function calculatePercentOfHight ()
 function percentGoUpButton()
 {
     let up = document.querySelector("#go-up"), // 获取按钮
-        percentResult = calculatePercentOfHight()
+        percentResult = calculatePercentOfHight();
 
-    if (percentResult 
+    if (percentResult < 100) {
+        up.childNodes[1].style.display = 'none'
+        up.childNodes[0].style.display = 'block'
+        up.childNodes[0].innerHTML = `${percentResult}%`;
+    } else {
+        up.childNodes[0].style.display = 'none'
+        up.childNodes[1].style.display = 'block'
+    }
+};
+
+window.onscroll = function ()
+{
+    percentGoUpButton()
+}; // 当滚动条有变化时候, 运行函数; 这么写方便以后添加新的要用到页面百分比高度的地方
+
